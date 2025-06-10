444cfe35fd086e6f3c486c2725bdfa4daa1868f2
Boredumdidum <boredumdidum10@gmail.com>
2025-06-10 12:32:55 +1000

    menu :)

--- zombie+vampire_game/Scripts/menu.gd
+++ zombie+vampire_game/Scripts/menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene("res://Scenes/world.tscn")
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 
 func _on_settings_button_pressed() -> void:
 	get_tree().change_scene()
