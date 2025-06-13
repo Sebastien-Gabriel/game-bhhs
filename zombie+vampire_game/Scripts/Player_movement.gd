8e5df1df861fb72fd74a6257fcd02904217f4d1c
Sebastien-Gabriel <sebastiengabriel16398@gmail.com>
2025-06-13 12:48:46 +1000

    Boutta crashout

--- zombie+vampire_game/Scripts/Player_movement.gd
+++ zombie+vampire_game/Scripts/Player_movement.gd
@@ -1,5 +1,7 @@
-
 extends CharacterBody2D
+
+# Declare a reference to the player
+var player
 
 const SPEED = 200
 const JUMP_VELOCITY = -400
@@ -17,8 +19,8 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		direction -= 1
 
-	velocity.x = direction * SPEED
 
+	velocity.x = direction * SPEED
 	# Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
